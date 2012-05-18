74544cf61141992267d6af602736f0846d184e32
PirosB3 <pirosb3@gmail.com>
2012-05-18 17:44:10 +0200

    started adding AUTH

--- settings/__init__.py
+++ settings/__init__.py
@@ -7,6 +7,10 @@ SECRET_KEY = '9r8h8239e7986h*/whefhefiwkjnkdcnjksa+)(Y£"'
 # Database settings
 DATABASE_NAME = 'recipes'
 
+# Auth settings
+AUTH_USERNAME = 'username'
+AUTH_PASSWORD = 'password'
+
 # Import local settings
 try:
     from local_settings import *
