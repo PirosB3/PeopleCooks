7781d42eec98fa3edc383975af936ca6ee021390
PirosB3 <pirosb3@gmail.com>
2012-07-03 12:28:58 +0200

    added backbone and started adding models

--- static/js/peoplecooks.js
+++ static/js/peoplecooks.js
@@ -12,3 +12,45 @@ var PC = (function(module){
   }
   return module;
 })(PC || {})
+
+// Initialization
+$(function() {
+    // Create Model
+    var Recipe= Backbone.Model.extend();
+    
+    // Create Collections
+    var RecipeCollection = Backbone.Collection.extend();
+
+    // Create Views
+    var RecipeView = Backbone.View.extend({
+        template: _.template('<a href="#<%= url %>"><%= name %></a>'),
+        el: 'li',
+        render: function() {
+            $(this.el).html(this.template({
+                name: this.model.get('name'),
+                url: escape(this.model.get('name'))
+            }));
+            return this;
+        }
+    });
+
+    var RecipeListView = Backbone.View.extend({
+        el: $('#recipeList'),
+        render: function() {
+            var that = this;
+            this.collection.forEach(function(recipe) {
+                var recipeView = new RecipeView({ model: recipe });
+                that.el.appendChild(recipeView.render().el);
+            });
+        }
+    });
+
+    // Start load
+    $.getJSON(PC.recipeNamesURL, function(res) {
+        var recipeCollection = new RecipeCollection(_.map(res.result, function(recipe) {
+            return new Recipe({ name: recipe });
+        }));
+        new RecipeListView({ collection: recipeCollection }).render();
+    });
+
+});
